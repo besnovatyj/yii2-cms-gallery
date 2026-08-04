3bd304f0b359bfff64fd6163cc5bbb6f1de9981d
besnovatyj <metr31@yandex.ru>
2026-08-05 00:46:26 +0500

    edit: 'id -> slug' on categories

--- src/controllers/frontend/GalleryController.php
+++ src/controllers/frontend/GalleryController.php
@@ -47,13 +47,13 @@ class GalleryController extends Controller
     }
 
     /**
-     * @param int $id
+     * @param string $slug
      * @return string
      * @throws NotFoundHttpException
      */
-    public function actionCategory(int $id): string
+    public function actionCategory(string $slug): string
     {
-        if (!$category = $this->categories->find($id)) {
+        if (!$category = $this->categories->findBySlug($slug)) {
             throw new NotFoundHttpException('The requested page does not exist.');
         }
 
